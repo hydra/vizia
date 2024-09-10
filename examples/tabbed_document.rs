acb0c5cd02eab1e36876bd35ab5996a53ae17a6e
Dominic Clifton <me@dominicclifton.name>
2024-09-10 15:20:08 +0200

    WIP 2

--- examples/tabbed_document.rs
+++ examples/tabbed_document.rs
@@ -2,6 +2,7 @@ mod helpers;
 pub use helpers::*;
 use vizia::prelude::*;
 use crate::document::Document;
+use crate::tabbed_ui::{DocumentTab, HomeTab, TabKind};
 
 mod document {
     use vizia_core::prelude::Data;
@@ -13,16 +14,82 @@ mod document {
     }
 }
 
+mod tabbed_ui {
+    use vizia::prelude::*;
+    use crate::document::Document;
+
+    #[derive(Clone, Data)]
+    pub enum TabKind {
+        Home(HomeTab),
+        Document(DocumentTab),
+    }
+
+    #[derive(Clone, Data)]
+    pub struct HomeTab {}
+
+    impl HomeTab {
+        pub fn build_tab(&self, name: String) -> TabPair {
+            let tab = TabPair::new(
+                move |cx| {
+                    Label::new(cx, name.clone()).hoverable(false);
+                    Element::new(cx).class("indicator");
+                },
+                |cx| {
+                    Element::new(cx).size(Percentage(100.0)).background_color(Color::rgb(0xbb, 0xbb, 0xbb));
+                },
+            );
+
+            tab
+        }
+    }
+
+    #[derive(Clone, Data)]
+    pub struct DocumentTab { pub document: Document }
+
+    impl DocumentTab {
+        pub fn build_tab(&self, name: String) -> TabPair {
+            let tab = TabPair::new(
+                move |cx| {
+                    Label::new(cx, name.clone()).hoverable(false);
+                    Element::new(cx).class("indicator");
+                },
+                |cx| {
+                    Element::new(cx).size(Percentage(100.0)).background_color(Color::rgb(0xdd, 0xdd, 0xdd));
+                },
+            );
+
+            tab
+        }
+    }
+
+    impl TabKind {
+        pub fn name(&self) -> String {
+            match self {
+                TabKind::Home(_) => { "Home".to_string() }
+                TabKind::Document(document_tab) => { document_tab.document.name.clone() }
+            }
+        }
+
+        pub fn build_tab(&self) -> TabPair {
+            match self {
+                TabKind::Home(tab) => tab.build_tab(self.name()),
+                TabKind::Document(tab) => tab.build_tab(self.name()),
+            }
+        }
+    }
+}
+
 #[derive(Lens, Default)]
 pub struct AppData {
-    documents: Vec<Document>,
+    tabs: Vec<TabKind>,
 }
 
 impl AppData {
-    pub fn load_documents(&mut self) {
-        self.documents.extend(vec![
-           Document { id: "document_1".to_string(), name: "Document 1".to_string() },
-           Document { id: "document_2".to_string(), name: "Document 2".to_string() },
+    pub fn create_tabs(&mut self) {
+        self.tabs.extend(vec![
+            TabKind::Home( HomeTab {} ),
+            TabKind::Document( DocumentTab {document: Document { id: "document_1".to_string(), name: "Document 1".to_string() }}),
+            TabKind::Document( DocumentTab {document: Document { id: "document_2".to_string(), name: "Document 2".to_string() }}),
         ]);
     }
 }
@@ -34,22 +101,12 @@ fn main() -> Result<(), ApplicationError> {
     Application::new(|cx| {
 
         let mut app_data = AppData::default();
-        app_data.load_documents();
+        app_data.create_tabs();
         app_data.build(cx);
 
         ExamplePage::new(cx, |cx| {
-            TabView::new(cx, AppData::documents, |cx, item| {
-                let tab = TabPair::new(
-                    move |cx| {
-                        Label::new(cx, item.map(|document|document.name.clone())).hoverable(false);
-                        Element::new(cx).class("indicator");
-                    },
-                    |cx| {
-                        Element::new(cx).size(Percentage(100.0)).background_color(Color::rgb(0xdd, 0xdd, 0xdd));
-                    },
-                );
-
-                tab
+            TabView::new(cx, AppData::tabs, |cx, tab_kind_lens| {
+                tab_kind_lens.get(cx).build_tab()
             })
                 .width(Percentage(100.0))
                 .height(Percentage(100.0));
